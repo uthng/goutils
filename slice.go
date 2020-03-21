c9197e7647a239237ab6659f92bda3ec62fe12cf
Thanh NGUYEN <u.nguyenthanh@gmail.com>
2020-03-21 18:41:30 +0100

    fix(slice): Only return position and use assert for unit tests

--- slice.go
+++ slice.go
@@ -8,25 +8,25 @@ import (
 type CompareFunc func(elem1 interface{}, elem2 interface{}) bool
 
 // SliceIsElemIn checks if an element is in an array of elements of the same type
-func SliceIsElemIn(elem interface{}, array []interface{}, fn CompareFunc) bool {
+func SliceIsElemIn(array []interface{}, elem interface{}, fn CompareFunc) int {
 
-	for _, elm := range array {
+	for i, elm := range array {
 		res := fn(elem, elm)
 		if res == true {
-			return true
+			return i
 		}
 	}
 
-	return false
+	return -1
 }
 
 // SliceFindElemStr takes a slice and looks for an element in it. If found it will
-// return the element's position and true. Otherwise it will return -1 and false
-func SliceFindElemStr(slice []string, val string) (int, bool) {
+// return the element's position. Otherwise it will return -1
+func SliceFindElemStr(slice []string, val string) int {
 	for i, item := range slice {
 		if item == val {
-			return i, true
+			return i
 		}
 	}
-	return -1, false
+	return -1
 }
--- slice_test.go
+++ slice_test.go
@@ -1,8 +1,8 @@
 package goutils_test
 
 import (
-	"fmt"
-	"strings"
+	//"fmt"
+	//"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,11 +11,25 @@ import (
 )
 
 func TestSliceIsElemIn(t *testing.T) {
-	var res bool
-	arrayStr := []interface{}{"toto", "titi", "tata", "tete"}
-	str1 := "titi"
-	str2 := "blabla"
-	testRes := false
+
+	testCases := []struct {
+		name   string
+		elem   string
+		result interface{}
+	}{
+		{
+			"ErrElemNotFound",
+			"mimi",
+			-1,
+		},
+		{
+			"OKElemFound",
+			"tata",
+			2,
+		},
+	}
+
+	sliceStr := []interface{}{"toto", "titi", "tata", "tete"}
 
 	fn := func(s1 interface{}, s2 interface{}) bool {
 
@@ -25,64 +39,38 @@ func TestSliceIsElemIn(t *testing.T) {
 		return false
 	}
 
-	res = goutils.SliceIsElemIn(str1, arrayStr, fn)
-	if res == true {
-		fmt.Printf("OK: \"%v\" is found in [%v]\n", str1, arrayStr)
-		testRes = true
-	} else {
-		fmt.Printf("ERR: \"%v\" is not found in [%v]\n", str1, arrayStr)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			pos := goutils.SliceIsElemIn(sliceStr, tc.elem, fn)
+			assert.Equal(t, tc.result, pos)
+		})
 	}
-
-	if testRes == false {
-		t.Fail()
-	}
-
-	res = goutils.SliceIsElemIn(str2, arrayStr, fn)
-	if res == true {
-		fmt.Printf("ERR: \"%v\" is found in [%v]\n", str2, arrayStr)
-	} else {
-		fmt.Printf("OK: \"%v\" is not found in [%v]\n", str2, arrayStr)
-		testRes = true
-	}
-
-	if testRes == false {
-		t.Fail()
-	}
-
 }
 
 func TestSliceFindElemStr(t *testing.T) {
 	testCases := []struct {
 		name   string
-		slice  []string
 		elem   string
 		result interface{}
 	}{
 		{
 			"ErrElemNotFound",
-			[]string{"toto", "titi", "tata", "tete"},
 			"mimi",
 			-1,
 		},
 		{
 			"OKElemFound",
-			[]string{"toto", "titi", "tata", "tete"},
 			"tata",
 			2,
 		},
 	}
 
+	sliceStr := []string{"toto", "titi", "tata", "tete"}
+
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			pos, ok := goutils.SliceFindElemStr(tc.slice, tc.elem)
-			if strings.HasPrefix(tc.name, "Err") {
-				assert.Equal(t, ok, false)
-				assert.Equal(t, pos, tc.result)
-				return
-			}
-
-			assert.Equal(t, ok, true)
-			assert.Equal(t, pos, tc.result)
+			pos := goutils.SliceFindElemStr(sliceStr, tc.elem)
+			assert.Equal(t, tc.result, pos)
 		})
 	}
 }
